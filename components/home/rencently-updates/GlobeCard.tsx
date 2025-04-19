d2d61afab49e2278b35fe0f12a40b16566f57e90
daat21 <daat21@icloud.com>
2025-04-20 05:00:48 +0930

    Update GlobeCard component to improve text responsiveness

--- components/home/rencently-updates/GlobeCard.tsx
+++ components/home/rencently-updates/GlobeCard.tsx
@@ -3,7 +3,7 @@ import { Globe } from '@/components/magicui/globe'
 export default function GlobeCard() {
   return (
     <div className="w-max-[348.25px] relative flex h-[400px] overflow-hidden rounded-lg border md:h-full">
-      <div className="m-4 flex flex-col gap-2">
+      <div className="m-4 flex flex-col gap-2 text-sm md:text-base">
         <p className="mt-0">📍 Current Location: Adelaide, SA, Australia</p>
         <p className="mt-0">✈️ Lived in Guangdong, China in past</p>
       </div>
