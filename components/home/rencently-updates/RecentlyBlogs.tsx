a328ec5358ee3ed9a1f7df8972f86a37d281fcb9
daat21 <daat21@icloud.com>
2025-04-23 04:41:56 +0930

    fix: blogs list sort method

--- components/home/rencently-updates/RecentlyBlogs.tsx
+++ components/home/rencently-updates/RecentlyBlogs.tsx
@@ -7,7 +7,7 @@ export async function RecentlyBlogs() {
   const { data: blogs, error } = await supabase
     .from('posts')
     .select('*')
-    .order('published_at', { ascending: false })
+    .order('updated_at', { ascending: false })
 
   if (error) {
     console.error(error)
