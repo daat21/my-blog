7656c50fd06f19efa3f316151139faafad1c9bd2
daat21 <daat21@icloud.com>
2025-04-24 12:38:37 +0930

    fix: improve redirect logic for authentication callback

--- app/(auth)/callback/route.ts
+++ app/(auth)/callback/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
 // The client you created from the Server-Side Auth instructions
 import { createClient } from '@/lib/supabase/server'
-import { getBaseUrl } from '@/lib/getUrl'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
@@ -13,12 +12,19 @@ export async function GET(request: Request) {
     const supabase = await createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      const baseUrl = getBaseUrl(request, origin)
-      return NextResponse.redirect(`${baseUrl}${next}`)
+      const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
+      const isLocalEnv = process.env.NODE_ENV === 'development'
+      if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+        return NextResponse.redirect(`${origin}${next}`)
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+      } else {
+        return NextResponse.redirect(`${origin}${next}`)
+      }
     }
   }
 
   // return the user to an error page with instructions
-  const baseUrl = getBaseUrl(request, origin)
-  return NextResponse.redirect(`${baseUrl}/auth/auth-code-error`)
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
 }
